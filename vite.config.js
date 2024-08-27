ace0ae7ce8b6a766a3d1ad19f83fd7990005322a
jatinbaniya13ix <agrawaljatin456@gmail.com>
2024-08-27 12:57:52 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ClientWebsite/', // Update this with your repo name
+  base: '/clientWebsite/', // Ensure this matches your GitHub repo name
   plugins: [react()],
-})
+});
